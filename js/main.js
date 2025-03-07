aebb611d357201ed378e573ebe97edfe58d97c28
Igor Matlingiewicz <igor.matlingiewicz@gmail.com>
2025-03-08 00:19:12 +0100

    Removed one comment

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const navItems = document.querySelector('.nav__items');
   
     toggleBtn.addEventListener('click', () => {
-      // Dodaj/usuń klasę --hidden
       navItems.classList.toggle('nav__items--hidden');
     });
   });
